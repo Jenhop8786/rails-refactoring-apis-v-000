ff27f0f34ef8291344c101c2abf698262a6a861a
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-09-06 17:45:17 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user
 
-  private
+  def logged_in?
+    !!session[:token]
+  end
 
-    def authenticate_user
-      redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo" if !logged_in?
-    end
-
-    def logged_in?
-      !!session[:token]
-    end
+  def authenticate_user
+    github_url = "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo"
+    redirect_to github_url unless logged_in?
+  end
+  
 end
